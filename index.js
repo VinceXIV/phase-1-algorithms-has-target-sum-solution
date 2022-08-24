73a0e9b1bc1de16753114473b3a160b9bdd28d7d
Vincent <vincent.makokha@student.moringaschool.com>
2022-08-24 17:35:00 +0300

    Completed Assignmet

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenVals = {}
+
+  for(number of array){
+    const complement = target - number;
+    if([complement] in seenVals){
+      return true
+    }else{
+      seenVals[number] = true
+    }
+  }
+
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  runtime: O(n)
+  space: O(1)
 */
 
 /* 
@@ -20,6 +34,12 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([4, 2, 5, 6, 0], 5));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([6, 7, 8, 2, 4], 6));
+
   console.log("");
 
   console.log("Expecting: true");
@@ -29,6 +49,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([6, 7, 3], 8));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5], 12));
 }
 
 module.exports = hasTargetSum;
